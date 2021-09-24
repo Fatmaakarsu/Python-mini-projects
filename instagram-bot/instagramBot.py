8fee1fc31c27afc7ed281eef0f4af266a1433896
Fatmaakarsu <20120205020@ismu.edu.tr>
2021-09-24 19:03:37 +0300

    update

--- instagram-bot/instagramBot.py
+++ instagram-bot/instagramBot.py
@@ -2,27 +2,6 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 
-# class Instagram():
-#     def __init__(self,userName,password):
-#         self.browser = webdriver.Chrome()
-#         self.username = userName
-#         self.password = password
-
-#     def sign(self):
-#         self.browser.get("https://www.github.com")
-#         time.sleep(3)
-
-#         userNameInput = self.browser.find_element_by_xpath("//*[@id='loginForm']/div/div[1]/div/label/input")
-#         passwordInput = self.browser.find_elements_by_xpath("//*[@id='loginForm']/div/div[2]/div/label/input")
-
-#         userNameInput.send_keys(self.userName)
-#         passwordInput.send_keys(self.password)
-#         passwordInput.send_keys(Keys.ENTER)
-
-    
-
-# insta = Instagram("helll","dnjkjcd")
-
 driver = webdriver.Chrome()
 driver.get("https://www.instagram.com/accounts/login/")
 time.sleep(2)
